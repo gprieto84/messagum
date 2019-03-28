39b8d22d4efebfa62f5f4bf1d4d98f7449f246f5
gprieto84 <german.prt@gmail.com>
2019-03-27 23:01:35 -0500

    Heroku pre

--- appdir/__init__.py
+++ appdir/__init__.py
@@ -32,7 +32,11 @@ def create_app(config_class=Config):
     from appdir.main import bp as main_bp
     app.register_blueprint(main_bp)
 
-    if not app.debug and not app.testing:
+    if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+    else:
         if not os.path.exists('logs'):
                 os.mkdir('logs')
 
--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'something really odd in the house'
     ENCRYPT_KEY = os.environ.get('ENCRYPT_KEY')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or "postgresql://localhost:5432/messagum"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
